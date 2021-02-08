5c68e2c210f1e196495a2759b2c05ff0aa0f73c8
terrellv <terrell.vest@gmail.com>
2021-02-07 22:51:41 -0500

    remove explicit use of credentials

--- func/decrypt.py
+++ func/decrypt.py
@@ -5,9 +5,7 @@ from base64 import b64decode
 
 def decrypt(encrypted_val):
     return boto3.client(
-        service_name='kms',
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+        service_name='kms'
     ).decrypt(
         CiphertextBlob=b64decode(encrypted_val),
         EncryptionContext={
